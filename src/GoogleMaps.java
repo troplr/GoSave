1ca5663a1e9cff0d65c018dac93bb72b178f94ea
Zhenghua Chen <calchen@huntsman-ve501-0378.apn.wlan.upenn.edu>
2019-02-02 21:56:52 -0500

    worked on GoogleMaps.java, first working prototype

--- src/GoogleMaps.java
+++ src/GoogleMaps.java
@@ -1,11 +1,9 @@
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
-import java.net.MalformedURLException;
 import java.net.URL;
 import java.net.URLConnection;
 import java.util.ArrayList;
-
 import org.json.JSONArray;
 import org.json.JSONException;
 import org.json.JSONObject;
@@ -26,7 +24,7 @@ public class GoogleMaps {
 		ArrayList<String> location = new ArrayList<String>();
 		String jsonTxt = "";
 		String queryParams = "address=" + address + "&key=" + GoogleAPISecretFile.key;
-		String urlStr = GoogleAPISecretFile.endPoint + GoogleAPISecretFile.staticMapsPath2
+		String urlStr = GoogleAPISecretFile.endPoint + GoogleAPISecretFile.geocodePath
 				+ queryParams;
 		
 		URL url = new URL(urlStr);
@@ -48,18 +46,30 @@ public class GoogleMaps {
         return location;
 	}
 	
-	public static ArrayList<Restaurant> searchNearby(ArrayList<String> location, String keyword) throws IOException, JSONException {
-		String jsonTxt = "";
-		String latitude = location.get(0);
-		String longitude = location.get(1);
+	public static ArrayList<String> findBest(String userAddr, String keyword, int dollarSign) throws IOException, JSONException {
+		String processedAddr = GoogleMaps.convertAddress(userAddr);
+		ArrayList<String> coordinates = GoogleMaps.getCoordinates(processedAddr);
+		String latitude = coordinates.get(0);
+		String longitude = coordinates.get(1);
+		int price;
+		if (dollarSign == 1) {
+			price = 1;
+		} else if (dollarSign == 2) {
+			price = 2;
+		} else if (dollarSign == 3) {
+			price = 3;
+		} else {
+			price = 4;
+		}
 		String queryParams = "location=" + latitude + "," + longitude +
-				"&radius=10000&type=restaurant&keyword=" + keyword +
-				"&key="+GoogleAPISecretFile.key;
-		String urlStr = GoogleAPISecretFile.endPoint + GoogleAPISecretFile.staticMapsPath1
+				"&radius=10000&type=restaurant&keyword=" + keyword + "&minprice=" + price +
+				"&maxPrice=" + price + "&rankby=prominence&key="+GoogleAPISecretFile.key;
+		String urlStr = GoogleAPISecretFile.endPoint + GoogleAPISecretFile.nearbySearchPath
 				+ queryParams;
 		URL url = new URL(urlStr);
         URLConnection urlConnection = url.openConnection();
 		BufferedReader in = new BufferedReader(new InputStreamReader(urlConnection.getInputStream()));
+		String jsonTxt = "";
 		String inputLine = in.readLine();
         while (inputLine != null) {
         	jsonTxt += inputLine;
@@ -67,18 +77,74 @@ public class GoogleMaps {
         }
         in.close();
 		
-		ArrayList<Restaurant> al = new ArrayList<Restaurant>();
         JSONObject jsonObj = new JSONObject(jsonTxt);
         JSONArray jsonArr = jsonObj.getJSONArray("results");
+        
+        ArrayList<String> al = new ArrayList<String>();
         for (int i = 0; i < jsonArr.length(); i++) {
-        	String name = jsonArr.getJSONObject(i).getString("name");
-        	Restaurant r = new Restaurant(name, 1.0, 1);
-        	al.add(r);
+        	try {
+        		String name = jsonArr.getJSONObject(i).getString("name");
+        		double priceLevel = jsonArr.getJSONObject(i).getDouble("price_level");
+        		double rating = jsonArr.getJSONObject(i).getDouble("rating");
+        		double ratio = rating / priceLevel;
+        		//System.out.println(ratio);
+        		al.add(name);
+        		al.add(Double.toString(ratio));
+        		break;
+        	} catch (JSONException e) {
+        		
+        	}
         }
         return al;
 	}
 	
-	public static double getDistance(String src, String dest) throws IOException, JSONException {
+	public static double calcAvg(String userAddr, String keyword) throws IOException, JSONException {
+		String processedAddr = GoogleMaps.convertAddress(userAddr);
+		ArrayList<String> coordinates = GoogleMaps.getCoordinates(processedAddr);
+		String latitude = coordinates.get(0);
+		String longitude = coordinates.get(1);
+		String queryParams = "location=" + latitude + "," + longitude +
+				"&radius=10000&type=restaurant&keyword=" + keyword +
+				"&rankby=prominence&key="+GoogleAPISecretFile.key;
+		String urlStr = GoogleAPISecretFile.endPoint + GoogleAPISecretFile.nearbySearchPath
+				+ queryParams;
+		URL url = new URL(urlStr);
+        URLConnection urlConnection = url.openConnection();
+		BufferedReader in = new BufferedReader(new InputStreamReader(urlConnection.getInputStream()));
+		String jsonTxt = "";
+		String inputLine = in.readLine();
+        while (inputLine != null) {
+        	jsonTxt += inputLine;
+        	inputLine = in.readLine();
+        }
+        in.close();
+		
+        double tot = 0;
+        int count = 0;
+        
+        JSONObject jsonObj = new JSONObject(jsonTxt);
+        JSONArray jsonArr = jsonObj.getJSONArray("results");
+        for (int i = 0; i < jsonArr.length(); i++) {
+        	try {
+        		double priceLevel = jsonArr.getJSONObject(i).getDouble("price_level");
+        		double rating = jsonArr.getJSONObject(i).getDouble("rating");
+        		double ratio = rating / priceLevel;
+        		//System.out.println(ratio);
+            	tot += ratio;
+            	count++;
+        	} catch (JSONException e) {
+        		
+        	}
+        }
+        
+        return tot / (double) count;
+	}
+	
+	public static int countPts(double avg, double ratio) {
+		return (int) Math.round(((ratio - avg) * 100 / avg));
+	}
+	
+	/*public static double getDistance(String src, String dest) throws IOException, JSONException {
 		String jsonTxt = "";
 		String processedSrc = GoogleMaps.convertAddress(src);
 		String processedDest = GoogleMaps.convertAddress(dest);
@@ -100,17 +166,16 @@ public class GoogleMaps {
         JSONArray jsonArr = jsonObj.getJSONArray("rows");
         Double distance = jsonArr.getJSONObject(0).getJSONArray("elements").getJSONObject(0).getJSONObject("distance").getDouble("value");
         return distance;
-	}
+	}*/
 	
 	public static void main(String[] args) {
 		try {
-			String processedStr = GoogleMaps.convertAddress("1600 Amphitheatre Parkway, Mountain View, CA");
-			ArrayList<String> location = GoogleMaps.getCoordinates(processedStr);
-			ArrayList<Restaurant> restaurants = GoogleMaps.searchNearby(location, "Chinese");
-			for (Restaurant r : restaurants) {
-				System.out.println(r.getName());
-			}
-			//System.out.println(GoogleMaps.getDistance("NYC", "DC"));
+			String userAddr = "1600 Amphitheatre Parkway, Mountain View, CA";
+			ArrayList<String> result = GoogleMaps.findBest(userAddr, "Italian", 2);
+			System.out.println(result.get(0));
+			double avg = GoogleMaps.calcAvg(userAddr, "Chinese");
+			int pts = GoogleMaps.countPts(avg, Double.parseDouble(result.get(1)));
+			System.out.println(pts);
 		} catch (IOException | JSONException e) {
 			e.printStackTrace();
 		}
