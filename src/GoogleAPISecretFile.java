795e2fd239a5940559d72d4537f408652fa6df6e
Zhenghua (Calvin) Chen <41121094+calchen01@users.noreply.github.com>
2019-02-04 15:10:17 -0500

    Update GoogleAPISecretFile.java

--- src/GoogleAPISecretFile.java
+++ src/GoogleAPISecretFile.java
@@ -4,6 +4,6 @@ public class GoogleAPISecretFile {
 	final static String nearbySearchPath = "/maps/api/place/nearbysearch/json?";
 	final static String geocodePath = "/maps/api/geocode/json?";
 	//final static String staticMapsPath3 = "/maps/api/distancematrix/json?";
-	final static String key = "AIzaSyDochteMNoVZ2K1vdQsEVbgiLEZMnjsLm8";
+	final static String key = "";//get your own key
 	
 }
